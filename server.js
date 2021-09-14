8f6ea97888ae7ef2f351f33f677180db6822041f
shaimasawai <shaima.alsawaie@gmail.com>
2021-09-14 18:20:38 -0400

    push worke

---
+++ server.js
@@ -0,0 +1,42 @@
+"use strict";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+app.use(cors());
+const PORT = 4318;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+});
+
+let handleWeather = async (req, res) => {
+  let lat = Number(req.query.lat);
+  let lon = Number(req.query.lon);
+  let city_name = req.query.city_name;
+  let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${process.env.WEATHERBIT_API_KEY}`;
+  let axiosResponse = await axios.get(URL);
+  let weatherData = axiosResponse.data;
+  let cleanedData = weatherData.data.map((item) => {
+    return new ForeCast(item.datetime, item.weather.description);
+  });
+  res.status(200).json(cleanedData);
+};
+
+app.get("/weather", handleWeather);
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
+
+class ForeCast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
